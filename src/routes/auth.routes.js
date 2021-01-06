fb80d8b43f897b1ac95c89650c888d6aae6d3f2b
AmiralBl3ndic <camille.briand@efrei.net>
2021-01-06 17:14:17 +0100

    feat: add email verification to register route

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const passport = require('passport');
+const IsEmail = require('isemail');
 const UserService = require('../services/user.service');
 
 router.post('/register', async (req, res) => {
@@ -19,19 +20,41 @@ router.post('/register', async (req, res) => {
 		});
 	}
 
-	const createdUser = await UserService.createUser(email, password);
-
-	if (!createdUser) {
-		return res.status(500).json({
+	if (!IsEmail.validate(email)) {
+		return res.status(400).json({
 			error: true,
-			reason: 'Unable to create user',
+			reason: `Invalid email address: ${email}`,
 		});
 	}
 
-	return res.status(201).json({
-		error: false,
-		user: createdUser,
-	});
+	try {
+		const createdUser = await UserService.createUser(email, password);
+
+		if (!createdUser) {
+			return res.status(500).json({
+				error: true,
+				reason: 'Unable to create user',
+			});
+		}
+
+		return res.status(201).json({
+			error: false,
+			user: createdUser,
+		});
+	} catch (err) {
+		// If email already being used
+		if (err.code && err.code === 'P2002') {
+			return res.status(409).json({
+				error: true,
+				reason: `Email ${email} is not available`,
+			});
+		}
+
+		return res.status(500).json({
+			error: true,
+			reason: 'Internal server error',
+		});
+	}
 });
 
 router.post('/login', passport.authenticate('local'));
